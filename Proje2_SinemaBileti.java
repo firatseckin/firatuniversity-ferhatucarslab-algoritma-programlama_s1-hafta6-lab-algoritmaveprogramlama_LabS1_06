a718547e93c55fc3d7da2ec8eb92964b1c4f146c
firatseckin <250541042@firat.edu.tr>
2025-11-27 22:41:14 +0300

    Update Proje2_SinemaBileti.java

---
+++ Proje2_SinemaBileti.java
@@ -0,0 +1,8 @@
+/* 
+ * Ad Soyad: Fırat Seçkin
+ * Öğrenci No: 250541042
+ * Proje Adı: Sinema Bileti
+ * Açıklama : Farklı gün, saat, yaş, meslek ve film formatlarına göre özel indirimler ve 
+ ek ücretler uygulayarak nihai sinema bileti fiyatını hesaplayan detaylı bir bilet satış sistemidir.
+ * Tarih: 27.11.2025
+ */
